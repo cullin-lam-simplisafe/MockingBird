50dbb828b113c8c865f43d75175d34e9e2d6e0ff
cullin-lam-simplisafe <cullin.lam@simplisafe.com>
2024-01-11 16:26:33 -0500

    fix: enable/ disable detection

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState, useRef, useEffect, useMemo } from 'react'
+import { useState, useRef, useEffect } from 'react'
 import './App.css'
 import { PoseLandmarker, FilesetResolver, DrawingUtils } from '@mediapipe/tasks-vision'
 import WebCam from 'react-webcam'
@@ -8,15 +8,12 @@ function App() {
   const webCamRef = useRef<WebCam>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const [detectionRunning, setDetectionRunning] = useState(false)
-  const lastVideoStartTimeRef = useRef(-1)
-  const canvasCtx = useMemo(() =>   canvasRef.current?.getContext("2d"),[canvasRef.current])
-  const drawingUtils = useMemo(() => canvasCtx ? new DrawingUtils(canvasCtx): null,[canvasCtx]) 
   const hasGetUserMedia = () => !!navigator.mediaDevices?.getUserMedia;
 
   // Load media pipe api
-  useEffect( () => {
+  useEffect(() => {
     FilesetResolver.forVisionTasks(
-      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.0/wasm"
+      "https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@latest/wasm"
     ).then(async (vision) => {
       poseLandmarkerRef.current = await PoseLandmarker.createFromOptions(vision, {
         baseOptions: {
@@ -28,19 +25,20 @@ function App() {
       });
     })
     setPoseLandmarkerReady(true)
-  },[])
+  }, [])
 
-  useEffect(() => { 
+  useEffect(() => {
     let id: number
+    const canvasCtx = canvasRef.current?.getContext("2d")
+    const drawingUtils = canvasCtx && new DrawingUtils(canvasCtx)
     function predictWebcam() {
-      if(canvasRef.current && webCamRef.current?.video && poseLandmarkerRef.current && canvasCtx && drawingUtils) {
+      if (canvasRef.current && webCamRef.current?.video && poseLandmarkerRef.current && canvasCtx && drawingUtils) {
         const canvasElement = canvasRef.current
         const video = webCamRef.current.video
         const poseLandmarker = poseLandmarkerRef.current
         // Now let's start detecting the stream.
         const startTimeMs = performance.now();
-        if (lastVideoStartTimeRef.current !== video.currentTime) {
-          lastVideoStartTimeRef.current = video.currentTime;
+        if (detectionRunning) {
           poseLandmarker.detectForVideo(video, startTimeMs, (result) => {
             canvasCtx.save();
             canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
@@ -56,34 +54,35 @@ function App() {
         }
       }
     }
-    if(detectionRunning) { 
+    if (detectionRunning) {
       predictWebcam()
     } else {
       const canvasElement = canvasRef.current
       canvasElement && canvasCtx?.clearRect(0, 0, canvasElement.width, canvasElement.height);
       canvasCtx?.save();
+
     }
-      return () => {
-       window.cancelAnimationFrame(id)
-      }
-  },[canvasCtx, detectionRunning, drawingUtils])
+    return () => {
+      window.cancelAnimationFrame(id)
+    }
+  }, [detectionRunning])
 
 
   return (
     <div>
-  <h1>MockingBird 🐦‍⬛</h1>
-    <h2>Intruder deterrence 🚨 powered by Artificial Intelligence 🤖</h2>
-    <p>Stand in front of your webcam to get real-time human detection.</p>
-        <div id="liveView" className="videoView">
+      <h1>MockingBird 🐦‍⬛</h1>
+      <h2>Intruder deterrence 🚨 powered by Artificial Intelligence 🤖</h2>
+      <p>Stand in front of your webcam to get real-time human detection.</p>
+      <div id="liveView" className="videoView">
         <button onClick={() => setDetectionRunning(!detectionRunning)} disabled={!hasGetUserMedia() || !poseLandmarkerReady} id="webcamButton" className="mdc-button mdc-button--raised">
           <span className="mdc-button__ripple"></span>
           <span className="mdc-button__label">{detectionRunning ? "DISABLE DETECTION" : "ENABLE DETECTION"}</span>
         </button>
-        <div style={{position: 'relative'}}>
-          <WebCam ref={webCamRef} id='webcam' style={{width: "480px", height: "360px", position: 'absolute'}}/>
-          <canvas ref={canvasRef} className="output_canvas" id="output_canvas" width="480px" height="360px" style={{position: 'absolute', left:"0px", top:'0px'}}></canvas>
+        <div style={{ position: 'relative' }}>
+          <WebCam ref={webCamRef} id='webcam' style={{ width: "480px", height: "360px", position: 'absolute' }} />
+          <canvas ref={canvasRef} className="output_canvas" id="output_canvas" width="480px" height="360px" style={{ position: 'absolute', left: "0px", top: '0px' }}></canvas>
+        </div>
       </div>
-    </div>
     </div>
   )
 }
